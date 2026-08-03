5d1ec7524836d7e44b37ab7a080f182e00481be1
Jitanshu <jitanshu.dev27@gmail.com>
2026-08-03 19:19:28 +0530

    Create-Home-Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 const App = () => {
   const location = useLocation()
@@ -8,6 +9,12 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+       <Routes >
+          <Route path='/'element={<Home/>}/>
+       </Routes>
+
+      </div>
     </div>
   )
 }
