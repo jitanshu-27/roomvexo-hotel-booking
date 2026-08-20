2c07c6bc289016053440b7539f3ea6ee62afab5e
Jitanshu <jitanshu.dev27@gmail.com>
2026-08-20 14:19:33 +0530

    Setup Clerk Authentication and create api for users

--- server/server.js
+++ server/server.js
@@ -2,11 +2,18 @@ import express from  "express"
 import "dotenv/config"
 import cors from "cors"
 import connectDB from "./configs/db.js"
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhook from "./controllers/clerkWebhooks.js"
 
 connectDB()
 const app = express()
 app.use(cors())
 
+// middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.use("/api/clerk", clerkWebhook);
 app.get("/" , (req , res)=>res.send("Api is Working fine everything."))
 
 
