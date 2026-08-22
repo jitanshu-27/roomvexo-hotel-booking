2f503d12b47b80b7b926b296378160db8a2d72bf
Jitanshu <jitanshu.dev27@gmail.com>
2026-08-22 14:57:12 +0530

    remove debug logs

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        console.log("URI Length:", process.env.MONGODB_URI?.length);
-        console.log("URI Prefix:", process.env.MONGODB_URI?.substring(0, 20));
-        console.log("VERCEL_ENV:", process.env.VERCEL_ENV);
+        
         mongoose.connection.on("connected", () => {
             console.log("Database Connected");
         });
