e48e56ca96dc3ea13765aba51b701456b581da27
Jitanshu <jitanshu.dev27@gmail.com>
2026-08-30 12:38:18 +0530

    fix: connect Experience and About navbar links to their respective sections

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -4,14 +4,14 @@ import { assets , exclusiveOffers} from '../assets/assets'
 
 const ExclusiveOffers = () => {
   return (
-    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30'>
+    <div id='experience' className='flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30'>
       <div className='flex flex-col md:flex-row items-center justify-between w-full'>
         <Title aling='left' title='Exclusie Offers' subTitle='Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.'/>
         <button className='group flex items-center gap-2 font-medium cursor-pointer max-md:mt-12'>
             View all offers
             <img src={assets.arrowIcon} alt="arrow-icons" className='group:hover:translate-x-1 transition-all'/>
         </button>
-      </div>
+      </div>Exclusive
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
         {exclusiveOffers.map((item)=>(
             <div key={item._id} className='group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center' style={{backgroundImage:`url(${item.image})`}}>
